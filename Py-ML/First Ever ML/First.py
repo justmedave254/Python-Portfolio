3dda9ea717d171adbe740c2b80f19666968103e1
_justmeDave <69865278+justmedave254@users.noreply.github.com>
2023-06-22 13:45:19 +0300

    made changes to the readme and first,py

--- Py-ML/First Ever ML/First.py
+++ Py-ML/First Ever ML/First.py
@@ -1,10 +1,11 @@
 import csv
-import pandas as pd
 from pandas import read_csv
+from pandas import set_option
 
 path1 = r"C:\Users\User\OneDrive\Documents\Code\Py ML\IRIS.csv"
 path2 = r"C:\Users\User\OneDrive\Documents\Code\Py ML\diabetes.csv"
 
+print("Prints data from the csv file and its shape")
 data = read_csv(path1)
 print(data.shape)
 print(data[:3])
@@ -13,3 +14,19 @@ print("-----------------------------")
 data2 = read_csv(path2)
 print(data2.shape)
 print(data2[:3])
+
+print("-----------------------------")
+print(data2.dtypes)
+
+print("-----------------------------")
+set_option('display.width', 100)
+set_option('display.precision', 2)  #up to 2dp
+print(data2.shape)
+print(data2.describe())
+
+
+print("-----------------------------")
+print("Prints the correlations between the pairs in the matrix")
+correlations = data2.corr(method='pearson')
+print(correlations)
+
